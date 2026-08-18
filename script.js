a318dfc82aa8ecf8b3b7027aabdfd1215a9906a8
Clayton Gann <cgann4@wgu.edu>
2026-08-18 18:00:56 -0500

    adds roll function and prints results to console

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ document.querySelector("#add-die") .addEventListener("click", function(addDie) {
     console.log("Die Added"); 
     const newSelect = document.createElement("select");
     const newOptionD4 = document.createElement("option");
-    newOptionD4.value = "D4";
+    newOptionD4.value = 4;
     newOptionD4.textContent = "D4";
     newSelect.appendChild(newOptionD4);
     const newOptionD6 = document.createElement("option");
-    newOptionD6.value = "D6";
+    newOptionD6.value = 6;
     newOptionD6.textContent = "D6";
     newSelect.appendChild(newOptionD6);
     const newOptionD8 = document.createElement("option");
-    newOptionD8.value = "D8";
+    newOptionD8.value = 8;
     newOptionD8.textContent = "D8";
     newSelect.appendChild(newOptionD8);
     const newOptionD10 = document.createElement("option");
-    newOptionD10.value = "D10";
+    newOptionD10.value = 10;
     newOptionD10.textContent = "D10";
     newSelect.appendChild(newOptionD10);
     const newOptionD12 = document.createElement("option");
-    newOptionD12.value = "D12";
+    newOptionD12.value = 12;
     newOptionD12.textContent = "D12";
     newSelect.appendChild(newOptionD12);
     const newOptionD20 = document.createElement("option");
-    newOptionD20.value = "D20";
+    newOptionD20.value = 20;
     newOptionD20.textContent = "D20";
     newSelect.appendChild(newOptionD20);
     document.querySelector("#select-container").appendChild(newSelect);
@@ -41,4 +41,14 @@ document.querySelector("#subtract-die") .addEventListener("click", function(subt
     selectContainer.removeChild(selectContainer.lastChild);
     NumDice = NumDice - 1;
    }
+});
+
+
+document.querySelector("#roll-button") .addEventListener("click", function(rollDice) {
+   document.querySelectorAll("#select-container select").forEach(function(select) {
+      console.log("Selected die:", select.value);
+      const dieValue = parseInt(select.value);
+      const roll = Math.floor(Math.random() * dieValue) + 1;
+      console.log("Roll result:", roll);
+   });
 });
